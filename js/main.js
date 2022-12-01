2f3f59fb74ca566f52efaa90754ba48c114617dd
trynceb <tryncebrandt@gmail.com>
2022-12-01 15:45:46 -0600

    1st draft

--- js/main.js
+++ js/main.js
@@ -1,62 +1,89 @@
 
+const spinBtnEl = document.querySelector(".spinBtn")
+const numberWindowEl = document.querySelector(".numberWindow")
+let oddsAre
+let theColorIs
+
+
+spinBtnEl.addEventListener("click", randomNum)
+spinBtnEl.addEventListener("click", evenOrOdd)
+spinBtnEl.addEventListener("click", blackOrRed)
+
+
+function randomNum() {
+    let randomNumber =  Math.floor(Math.random() * 36) + 1
+    numberWindowEl.innerHTML = randomNumber
+    return randomNumber
+}
+
+function evenOrOdd() {
+    let isEven = numberWindowEl.textContent
+    if (isEven % 2 == 0) {
+        oddsAre = "even"
+    } else {
+        oddsAre = "odd"
+    }
+}
+
+function blackOrRed() {
+    let number = numberWindowEl.textContent
+    let isOdd = oddsAre
+    let backColor
+    if (number > 0 && number < 11 && isOdd == "odd") {
+        theColorIs = "red"
+        backColor = "#D60000"
+    } else if (number > 10 && number < 19 && isOdd == "even") {
+        theColorIs = "red"
+        backColor = "#D60000"
+    } else if (number > 18 && number < 29 && isOdd == "odd") {
+        theColorIs = "red"
+        backColor = "#D60000"
+    } else if (number > 28 && number < 37 && isOdd == "even") {
+        theColorIs = "red"
+        backColor = "#D60000"
+    } else {
+        theColorIs = "black"
+        backColor = "#232323"
+    }
+    numberWindowEl.style.backgroundColor = backColor
+}
 
 // Create an array for the roulette board 0-36 + 00
 
 // Create arrays for:
 
 // evens and odds
-    if (number % 2 === 0) {
-        return even
-    } else {
-        return odd
-    }
 
-
-// black or red
-let blackOrRed =  function 
-if (number > 0 && number < 11 && number === odd) {
-    return red
-} else if (number > 18 && number < 29 && number === odd) {
-    return red
-} else if (number > 10 && number < 19 && number === even) {
-    return red
-} else if (number > 28 && number < 37 && number === even) {
-    return red
-} else if (number === 0 || number === 00) {
-    return green
-} else {
-    return black
-}
 
 // 1st 12, 2nd 12, 3rd 12
 
-if (number > 0 && number < 13) {
-    return 1st 12
-} else if (number > 12 && number < 25) {
-    return 2nd 12
-} else if (number > 24 && number < 37) {
-    return 3rd 12
-} else {
-    return null
-}
+// if (number > 0 && number < 13) {
+//     return 1st 12
+// } else if (number > 12 && number < 25) {
+//     return 2nd 12
+// } else if (number > 24 && number < 37) {
+//     return 3rd 12
+// } else {
+//     return null
+// }
 
 
 // making a class for all the numbers
-class PossibleNumbers {
-    constructor(number) {
-        this.number = number
-        this.evenOrOdd = PossibleNumbers.evenOrOdd(number)
-        this.color = 
-        this.thirds = 
-      }
-      static evenOrOdd(num) {
-        if (num % 2 === 0) {
-            return even
-        } else {
-            return odd
-        }
-      }
-}
+// class PossibleNumbers {
+//     constructor(number) {
+//         this.number = number
+//         this.evenOrOdd = PossibleNumbers.evenOrOdd(number)
+//         this.color = 
+//         this.thirds = 
+//       }
+//       static evenOrOdd(num) {
+//         if (num % 2 === 0) {
+//             return even
+//         } else {
+//             return odd
+//         }
+//       }
+// }
 
 
 
