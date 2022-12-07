97f376f064dac2a3bd7a1ee570e784930ddd8eb3
trynceb <tryncebrandt@gmail.com>
2022-12-06 20:59:22 -0600

    fixing css + js

--- js/main.js
+++ js/main.js
@@ -28,14 +28,7 @@ evenEl.addEventListener("click", placeBetEven)
 oddEl.addEventListener("click", placeBetOdd)
 numberInputEl.addEventListener("input", stringToNumber)
 
-// animation to run though colors in number window
-// $(".spinBtn").click(function() {
-//     $('.numberWindow').toggleClass('numberWindow-active');
-//   });
 
-
-console.log(numberInputEl.value)
-console.log(bankAccount)
 // activates spin wheel button, calls all the functions after bet is placed
 function spinTheWheel() {
     if (redChipEl.style.zIndex === "4" || blackChipEl.style.zIndex === "4" || evenChipEl.style.zIndex === "4" || oddChipEl.style.zIndex === "4") {
@@ -62,8 +55,7 @@ function randomNum() {
 
 // splits the value of the input accross both bets
 function splittingTheBet() {
-    if (winLoseEl.innerHTML === "Sorry, you lose this bet." && oddsResultEl.innerHTML === "Sorry, you lose this bet.") {
-    } else if (redChipEl.style.zIndex === "4" && oddChipEl.style.zIndex === "4") {
+    if (redChipEl.style.zIndex === "4" && oddChipEl.style.zIndex === "4") {
         numberInputEl.value /= 2
     } else if (redChipEl.style.zIndex === "4" && evenChipEl.style.zIndex === "4") {
         numberInputEl.value /= 2
@@ -124,23 +116,15 @@ function placeBetBlack() {
 
 // runs if you bet red win/lose then adds or subtracts betting value from bank acount
 function bettingRed() {
-    console.log("this is bankAccount before red", bankAccount)
-    console.log("this is numberInput before red", numberInputEl.value)
     if (redChipEl.style.zIndex === "4" && theColorIs === "red") {
         winLoseEl.innerHTML = "It's red! You win!"
         accountEl.innerHTML = parseInt(bankAccount) + parseInt(numberInputEl.value)
         bankAccount = parseInt(bankAccount) + parseInt(numberInputEl.value)
     } else if (redChipEl.style.zIndex === "4" && theColorIs === "black") {
-<<<<<<< HEAD
-        winLoseEl.innerHTML = "Sorry, you lose this bet."
-=======
         winLoseEl.innerHTML = "I'm sorry, you lose this bet."
->>>>>>> main
         accountEl.innerHTML = parseInt(bankAccount) - numberInputEl.value
         bankAccount = parseInt(bankAccount) - parseInt(numberInputEl.value)
         numberInputEl.value -= numberInputEl.value
-        console.log("this is bankAccount after red", bankAccount)
-        console.log("this is numberInput after red", numberInputEl.value)
     }
     redChipEl.style.zIndex = "0"
 
@@ -154,11 +138,7 @@ function bettingBlack() {
         accountEl.innerHTML = parseInt(bankAccount) + parseInt(numberInputEl.value)
         bankAccount = parseInt(bankAccount) + parseInt(numberInputEl.value)
     } else if (blackChipEl.style.zIndex === "4" && theColorIs === "red") {
-<<<<<<< HEAD
-        winLoseEl.innerHTML = "Sorry, you lose this bet."
-=======
         winLoseEl.innerHTML = "I'm sorry, you lose this bet."
->>>>>>> main
         accountEl.innerHTML = parseInt(bankAccount) - numberInputEl.value
         bankAccount = parseInt(bankAccount) - parseInt(numberInputEl.value)
         numberInputEl.value -= numberInputEl.value
@@ -180,51 +160,34 @@ function placeBetOdd() {
 
 // runs if you bet even win/lose then adds or subtracts betting value from bank acount
 function bettingEven() {
-    console.log("this is bankAccount before even", bankAccount)
-    console.log("this is numberInput before even", numberInputEl.value)
     if (evenChipEl.style.zIndex === "4" && oddsAre === "even") {
         oddsResultEl.innerHTML = "It's even! You win!"
         // numberInputEl.value *= 2
         accountEl.innerHTML = parseInt(bankAccount) + parseInt(numberInputEl.value)
         bankAccount = parseInt(bankAccount) + parseInt(numberInputEl.value)
     } else if (evenChipEl.style.zIndex === "4" && oddsAre === "odd") {
-<<<<<<< HEAD
-        oddsResultEl.innerHTML = "Sorry, you lose this bet."
-=======
         oddsResultEl.innerHTML = "I'm sorry, you lose this bet."
->>>>>>> main
         accountEl.innerHTML = parseInt(bankAccount) - numberInputEl.value
         bankAccount = parseInt(bankAccount) - parseInt(numberInputEl.value)
         numberInputEl.value -= numberInputEl.value
-        console.log("this is bankAccount after even", bankAccount)
-        console.log("this is numberInput after even", numberInputEl.value)
     }
     evenChipEl.style.zIndex = "0"
 }
 
 // runs if you bet odd win/lose then adds or subtracts betting value from bank acount
 function bettingOdd() {
-    console.log("this is the HTML", accountEl.innerHTML)
-    console.log("this is the bank account", bankAccount)
     if (oddChipEl.style.zIndex === "4" && oddsAre === "odd") {
         oddsResultEl.innerHTML = "It's odd! You win!"
         numberInputEl.value *= 2
         accountEl.innerHTML = parseInt(bankAccount) + parseInt(numberInputEl.value) 
         bankAccount = parseInt(bankAccount) + parseInt(numberInputEl.value)
     } else if (oddChipEl.style.zIndex === "4" && oddsAre === "even") {
-<<<<<<< HEAD
-        oddsResultEl.innerHTML = "Sorry, you lose this bet."
-=======
         oddsResultEl.innerHTML = "I'm sorry, you lose this bet."
->>>>>>> main
         accountEl.innerHTML = parseInt(bankAccount) - numberInputEl.value
         bankAccount = parseInt(bankAccount) - parseInt(numberInputEl.value)
         numberInputEl.value -= numberInputEl.value
     }
     oddChipEl.style.zIndex = "0"
-    console.log(numberInputEl.value)
-    console.log("this is the HTML", accountEl.innerHTML)
-    console.log("this is the bank account", bankAccount)
 }
 
 // Handle number changes for the input bet
@@ -246,27 +209,3 @@ function doubleLoss() {
 function refreshPage(){
     window.location.reload();
 } 
-
-
-    // add an input
-    //Choose bet, max amount cannot > than your amount and cannot be less than $1.
-
-//your amount starts at $100
-//betting amount = amount enter
-// your amount = your amount - betting amount
-//if win  your amount = your amount + (betting amount * 2)
-
-
-
-// 1st 12, 2nd 12, 3rd 12
-
-// if (number > 0 && number < 13) {
-//     return 1st 12
-// } else if (number > 12 && number < 25) {
-//     return 2nd 12
-// } else if (number > 24 && number < 37) {
-//     return 3rd 12
-// } else {
-//     return null
-// }
-
